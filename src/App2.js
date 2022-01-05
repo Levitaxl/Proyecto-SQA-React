363f5ab1162e16c6f383a9541990fddd617f206f
levitaxl <hermessamuel10@gmail.com>
2022-01-05 19:33:38 -0400

    first commit

---
+++ src/App2.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeTienda from './tiendas/src/pages/Home'
+import ProductList from './tiendas/src/pages/ProductList'
+import Product from './tiendas/src/pages/Product'
+import Register from './tiendas/src/pages/Register'
+import Login from './tiendas/src/pages/Login'
+
+function App2() {
+  return (
+    <Router>
+        <Switch>
+          <Route path="/home">
+            <HomeTienda />
+          </Route>
+
+          <Route path="/productsList">
+            <ProductList />
+          </Route>
+
+
+          <Route path="/tienda/producto">
+            <Product />
+          </Route>
+
+          <Route path="/tienda/register">
+            <Register />
+          </Route>
+
+          <Route path="/tienda/login">
+            <Login />
+          </Route>
+        </Switch>
+    </Router>
+    
+  );
+}
+
+export default App2;
